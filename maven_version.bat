002af4be63e400900cb929bd7c4d8d466a8522b5
MaxKey <shimingxy@qq.com>
2023-04-01 10:19:52 +0800

    3.0.1

---
+++ maven_version.bat
@@ -0,0 +1,6 @@
+set  MAVEN_HOME=D:\IDE\apache-maven-3.9.1
+set  JAVA_HOME=D:\IDE\jdk-17.0.2.8
+
+call %MAVEN_HOME%/bin/mvn versions:set -DnewVersion=3.0.1
+
+call %MAVEN_HOME%/bin/mvn versions:update-child-modules
