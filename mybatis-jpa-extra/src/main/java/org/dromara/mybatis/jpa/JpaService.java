8ecf838daf7e624ca85c539a11801b728fa98826
shimingxy <shimingxy@qq.com>
2024-08-26 15:34:52 +0800

    Update JpaService.java

--- mybatis-jpa-extra/src/main/java/org/dromara/mybatis/jpa/JpaService.java
+++ mybatis-jpa-extra/src/main/java/org/dromara/mybatis/jpa/JpaService.java
@@ -81,7 +81,7 @@ public  class  JpaService <T extends JpaEntity> {
 		} else {
 			logger.error("invalide initail, need generic type parameter! ");
 		}
-		logger.trace("Mapper {} , Entity Class : {}" , cls.getSimpleName(),entityClass.getSimpleName());
+		logger.trace("Mapper {} , Entity : {}" , String.format("%-20s", cls.getSimpleName()),entityClass.getSimpleName());
 	}
 
 	/**
