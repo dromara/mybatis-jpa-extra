5f74745f576c8a266b22d4b9e0b8ba3f3a9ba0c3
shimingxy <shimingxy@qq.com>
2025-02-14 16:18:27 +0800

    3.3.4

--- maven_version.bat
+++ maven_version.bat
@@ -1,6 +1,6 @@
 set  MAVEN_HOME=C:\ide\apache-maven-3.8.8
 set  JAVA_HOME=C:\ide\jdk-17.0.9+9
 
-call %MAVEN_HOME%/bin/mvn versions:set -DnewVersion=3.3.3
+call %MAVEN_HOME%/bin/mvn versions:set -DnewVersion=3.3.4
 
 call %MAVEN_HOME%/bin/mvn versions:update-child-modules
